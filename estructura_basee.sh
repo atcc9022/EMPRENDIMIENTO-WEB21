19d88a06bce5254685db3a0b6b84197a2149f715
alejandra <atcc9022@gmail.com>
2021-08-15 22:04:48 -0400

    VERSION FINAL

---
+++ estructura_basee.sh
@@ -0,0 +1,13 @@
+#!/bin/bash	
+touch index.html
+mkdir assets
+cd assets
+mkdir css
+cd css
+touch main.css
+cd ..
+mkdir img
+mkdir sass
+cd sass
+mkdir abstracts base components layout pages theme vendors
+touch main.scss
